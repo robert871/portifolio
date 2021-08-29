fa547e0f3fe9a7f97b79f000df58c21a63fbb91e
Roberto Lopes da Costa <71911175+robert871@users.noreply.github.com>
2021-08-29 00:41:48 -0300

    Add files via upload
    criação do primeiro portifolio

---
+++ script.js
@@ -0,0 +1,17 @@
+function teste(){
+    var el2=window.document.getElementById(`ul`)
+    el2.classList.replace("ul","ul1")
+    var el4=window.document.getElementById(`close`)
+    el4.classList.replace("close","close1")
+    
+ 
+ 
+ }
+ function list(){
+    var el3=window.document.getElementById(`ul`)
+    el3.classList.replace("ul1","ul")
+    var el4=window.document.getElementById(`close`)
+    el4.classList.replace("close1","close")
+    
+ 
+ }
